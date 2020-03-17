01530470d19d57b928232dc0f8b4b544588cd363
Evgeny Papazyan <swordelf@mail.ru>
2020-03-17 20:04:21 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+all: compprac2
+
+CC=g++
+LD=g++
+CFLAGS=-g -std=gnu++17 -Wall -Wextra -Iqbe/include
+LDFLAGS=-Lqbe/lib
+
+main.o: main.cpp
+	$(CC) -c $(CFLAGS) $(LDFLAGS) $< -o $@
+
+compprac2: main.o
+	$(LD) $(CFLAGS) $(LDFLAGS) $^ -o $@ -lqbe
+
+clean:
+	rm -f main.o compprac1
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#ifdef __cplusplus
+#define export exports
+extern "C" {
+#include <qbe/all.h>
+}
+#undef export
+#else
+#include <qbe/all.h>
+#endif
+
+#include <stdio.h>
+
+static int useful(Ins* i) {
+    // ...
+}
+
+static void readfn (Fn *fn) {
+    fillrpo(fn); // Traverses the CFG in reverse post-order, filling blk->id.
+    fillpreds(fn);
+    filluse(fn);
+    ssa(fn);
+
+    //...
+    for (Blk *blk = fn->start; blk; blk = blk->link) {
+        for (Ins *i = blk->ins; i < &blk->ins[blk->nins]; ++i) {
+            if (!useful(i)) {
+                i->op = Onop;
+                i->to = R;
+                i->arg[0] = R;
+                i->arg[1] = R;
+            }
+        }
+
+        std::cout << blk->name << std::endl;
+        // ...
+    }
+
+    fillpreds(fn); // Either call this, or keep track of preds manually when rewriting branches.
+    fillrpo(fn); // As a side effect, fillrpo removes any unreachable blocks.
+    printfn(fn, stdout);
+}
+
+static void readdat (Dat *dat) {
+  (void) dat;
+}
+
+int main () {
+  parse(stdin, "<stdin>", readdat, readfn);
+  freeall();
+}
